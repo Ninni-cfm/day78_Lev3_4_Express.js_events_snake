6d7288a5a3fb25d479d111b21912165d0ba2f949
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-10-06 15:54:38 +0200

    V2

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,3 +4,11 @@ function onMouseOver(div) {
         div.style.backgroundColor = "lightblue";
     }, 500);
 }
+function onMouseEnter(div) {
+    div.style.backgroundColor = "red";
+}
+function onMouseLeave(div) {
+    setTimeout(() => {
+        div.style.backgroundColor = "lightblue";
+    }, 500);
+}
